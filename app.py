4f2d37f3e61c97eb1a5216b7e08e1fa8ddbd3505
Ian Hoang <IanHoang@Iansteins-MacBook-Pro.local>
2020-07-17 23:17:10 -0500

    added logger and separated foodiebot into Sanji.py and App.py

--- app.py
+++ app.py
@@ -4,33 +4,53 @@ from slackeventsapi import SlackEventAdapter
 from flask import Flask
 import json
 from sanji import Sanji
+import logging
+
+#create logger, set level, set handler, set formatter
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARN)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('app.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 tokens = {}
 with open('configs.json') as jsonData:
-    tokens = json.load(jsonData);
+    tokens = json.load(jsonData)
+
+app = Flask(__name__)
 
 #Slackeventadapter documentation = SlackEventAdapter(TOKEN, Endpoint="/slack/events")
-slack_event_adapter = SlackEventAdapter(tokens.get("SLACK_SIGNING_SECRET"), "/slack/events")
+slack_event_adapter = SlackEventAdapter(tokens.get("SLACK_SIGNING_SECRET"), "/slack/events", app)
 #slackwebclient documentation = WebClient(TOKEN)
 slack_web_client = WebClient(token=tokens.get("SLACK_BOT_TOKEN"))
-
+logger.warning(f"slackeventadapter {slack_event_adapter} and slackwebclient {slack_web_client}")
 
 #========MESSAGE EVENTS=======
 # handle messages
 @slack_event_adapter.on("message")
 def getMessage(payload):
-    event = payload["event"]
-    channel = event["channel"]
+    #is there a reason why event = payload["event"] doesn't work?
+    #return {} if event does not exist
+    logger.warning(event = payload.get("event", {}))
+    # channel_id = event.get("channel")
+    # user_id = event.get("user")
+    # text = event.get("text")
+    # print(text)
+    # channel = event["channel"]
+    # text = event["text"]
 
-    #initialize Sanji
-    bot = Sanji(channel)
-    message = bot.handleMessage()
+    # #initialize Sanji
+    # bot = Sanji(channel)
+    # message = bot.handleMessage()
 
-    slack_web_client.chat_postMessage(message)
+    # slack_web_client.chat_postMessage(message)
 
 
 if __name__ == "__main__":
-    slack_event_adapter.start(port=3000)
+    app.run(port=3000)
 
 
 
